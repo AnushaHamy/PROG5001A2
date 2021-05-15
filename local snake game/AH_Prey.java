acae17d1f43c1722a356e0f0ebbece07b086d961
AnushaHamy <83069124+AnushaHamy@users.noreply.github.com>
2021-05-16 01:51:11 +1000

    Revert "Revert "Assignment 2 - Task 4 - Week 11 - Answer""
    This reverts commit 3fa186e21e757a1b1220fa0b836fca2baf0973ef.

--- local snake game/AH_Prey.java
+++ local snake game/AH_Prey.java
@@ -1,15 +1,32 @@
+
+import java.awt.Image;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel; 
+import javax.swing.ImageIcon; 
+import javax.swing.JFrame; 
 /**
  * Class Prey represents any prey object.
  *
  * @author Anusha Hamy
- * @version 1.0
+ * @version 4.0
  */
-public class AH_Prey {
+public class AH_Prey extends JPanel{
+    
+       Image img;
+       private int prey_x;
+       private int prey_y;
+       
+       private int dots;
+    
+       private final int DOT_SIZE = 30;
+       private final int RAND_POS = 29;
     /**
      * Constructor for objects of class Prey
      */
     public AH_Prey()
     {
+         img = new ImageIcon("images/prey.jpg").getImage();
     }   
     
     /**
@@ -19,7 +36,27 @@ public class AH_Prey {
     
     }
     
-   
+    /**
+     * locate the prey in random location
+     */
+        public void locatePrey() {
+
+        int r = (int) (Math.random() * RAND_POS);
+        prey_x = ((r * DOT_SIZE));
+
+        r = (int) (Math.random() * RAND_POS);
+        prey_y = ((r * DOT_SIZE));
+    }
+    
+       /**
+     * load prey random in board
+     */
+    public void doDrawing(Graphics g){
+        
+        g.drawImage(img, prey_x, prey_y, this);
+    
+    }
+    
 }
 
 
--- local snake game/AH_Snake.java
+++ local snake game/AH_Snake.java
@@ -1,16 +1,25 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
 /**
  * Write a description of class Snake here.
  *
  * @author Anusha Hamy
- * @version 1.0
+ * @version 4.0
  */
 public class AH_Snake
 {
+    
+    Image img_snake;
+    Image img_body;
+
     /**
      * Constructor for objects of class Snake
      */
     public AH_Snake() {
-        // initialise instance variables        
+        // initialise instance variables     
+          img_snake = new ImageIcon("images/head.png").getImage();
+        img_body = new ImageIcon("images/body.png").getImage();
     }
 
     /**
